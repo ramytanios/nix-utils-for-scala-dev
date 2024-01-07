0b0d14f01ca43f13d0cc44e6a1d2c3d6daeff44b
Ramy Tanios <ramy.tanios@gmail.com>
2024-01-07 10:27:08 +0100

    wip

---
+++ templates/scala-cli-app/flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Hello world Scala app";
+  inputs = {
+    typelevel-nix.url = "github:typelevel/typelevel-nix";
+    nixpkgs.follows = "typelevel-nix/nixpkgs";
+    flake-utils.follows = "typelevel-nix/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, typelevel-nix, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ typelevel-nix.overlay ];
+        };
+      in {
+        devShells = pkgs.devshell.mkShell {
+          imports = [ typelevel-nix.typelevelShell ];
+          name = "app-dev-shell";
+          typelevelShell = {
+            jdk.package = pkgs.jdk;
+            nodejs.enable = true;
+            native.enable = true;
+            native.libraries = [ pkgs.zlib pkgs.s2n-tls pkgs.openssl ];
+          };
+          packages = with pkgs; [ which ];
+        };
+      });
+}
