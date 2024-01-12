db89f500abec39ecefb1d7afbcd2527f5ffe17a3
Ramy Tanios <ramy.tanios@gmail.com>
2024-01-12 12:27:26 +0100

    fix flake

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     # templates for developing scala apps
     templates = {
       scala-cli-app = {
-        src = ./templates/scala-cli-app;
+        path = ./templates/scala-cli-app;
         description = "A scala app built with scala-cli";
       };
     };
